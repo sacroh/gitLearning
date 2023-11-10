f5d82b1005140c4d91b83017fc9acb06318fd44d
sacroh <pradeeptk07082000@gmail.com>
2023-11-10 22:52:30 +0530

    logtrace content added

--- app/logTrace.cpp
+++ app/logTrace.cpp
@@ -1,1 +1,21 @@
 // This is the sample file for learning cpp and git
+#include "iostream"
+
+int update()
+{
+    return 10;
+}
+
+int main()
+{
+    int a;
+    a = update();
+    if(a==5)
+    {
+        return a;
+    }
+    else
+    {
+        return a-1;
+    }
+}
